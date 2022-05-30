423ac9943b92064a01cc00bb0c07d937b3e24f47
Yago <peixinhoyago@gmail.com>
2022-05-30 17:23:19 -0300

    Desenvolvimento do CSS, criação do estilo na página

---
+++ javascript/script.js
@@ -0,0 +1,8 @@
+function openModal(){
+    document.getElementById("modal").classList.add("modal-active")
+}
+
+function closeModal(){
+    document.getElementById("modal").classList.remove('modal-active')
+}
+
