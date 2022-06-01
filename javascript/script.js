781667570bd979b02dbd746298cd5e04e953b8b6
Yago <peixinhoyago@gmail.com>
2022-06-01 18:51:48 -0300

    Criação do CREATE

--- javascript/script.js
+++ javascript/script.js
@@ -1,8 +1,41 @@
-function openModal(){
-    document.getElementById("modal").classList.add("modal-active")
+function openModal() {
+  document.getElementById("modal").classList.add("modal-active");
 }
 
-function closeModal(){
-    document.getElementById("modal").classList.remove('modal-active')
+function closeModal() {
+  document.getElementById("modal").classList.remove("modal-active");
 }
 
+function getLocalStorageItens() {
+  return JSON.parse(localStorage.getItem("localStorageAnimalList")) ?? [];
+}
+
+function create() {
+  const tutor = document.getElementById("tutor");
+  const animal = document.getElementById("animal");
+  const raca = document.getElementById("raca");
+  const idade = document.getElementById("idade");
+  const emailTutor = document.getElementById("email-tutor");
+  const telefoneTutor = document.getElementById("telefone-tutor");
+  const bairroTutor = document.getElementById("bairro-tutor");
+  const adotado = document.getElementById("adotado");
+
+  const newItem = {
+    tutor: tutor.value,
+    animal: animal.value,
+    raca: raca.value,
+    idade: idade.value,
+    emailTutor: emailTutor.value,
+    telefoneTutor: telefoneTutor.value,
+    bairroTutor: bairroTutor.value,
+    adotado: adotado.checked,
+  };
+
+  const localStorageResponse = getLocalStorageItens();
+  localStorageResponse.push(newItem);
+
+  localStorage.setItem(
+    "localStorageAnimalList",
+    JSON.stringify(localStorageResponse)
+  );
+}
